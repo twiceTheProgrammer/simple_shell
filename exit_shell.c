9394edc6026759e2a99727989e06bb35f50f1f48
Twice <njabulo.magagula@ribccs.com>
2023-05-24 17:38:39 +0200

    fixed undefined refs

--- exit_shell.c
+++ exit_shell.c
@@ -16,7 +16,7 @@ int exit_shell(data_shell *datash)
 	if (datash->args[1] != NULL)
 	{
 		ustatus = _atoi(datash->args[1]);
-		is_digit = _isdigit(datash->args[1]);
+		is_digit = isdigit(datash->args[1]);
 		str_len = strlen(datash->args[1]);
 		big_number = ustatus > (unsigned int)INT_MAX;
 		if (!is_digit || str_len > 10 || big_number)
--- main.h
+++ main.h
@@ -12,6 +12,7 @@
 #include <signal.h>
 #include <limits.h>
 #include <string.h>
+#include <ctype.h>
 
 #define BUFSIZE 1024
 #define TOK_BUFSIZE 128
@@ -103,7 +104,6 @@ void _memcpy(void *newptr, const void *ptr, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 int cmp_chars(char str[], const char *delim);
-int _isdigit(const char *s);
 void reverse_string(char *s);
 int repeated_char(char *input, int i);
 int error_sep_op(char *input, int i, char last);
